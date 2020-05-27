ee567a7fb2487d53dc9eb9ac3b983bda837cda2b
Abraham Meza <abraham.meza@sciodesk.com>
2020-05-27 10:02:04 -0500

    Comenzada nueva pagina de sciodesk

---
+++ BeautifulUI/BeautifulUI/BeautifulUI/ViewModels/SciodeskViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace BeautifulUI.ViewModels
+{
+    public class SciodeskViewModel : BaseViewModel
+    {
+        public ICommand SkeletonCommand => new Command(async () =>
+        {
+            IsBusy = true;
+            await Task.Delay(4000);
+            IsBusy = false;
+        });
+
+        public ICommand RefreshCommand => new Command(async () => await OnRefreshAsync());
+
+        private async Task OnRefreshAsync()
+        {
+            IsBusy = true;
+            await Task.Delay(3000);
+            IsBusy = false;
+        }
+    }
+}
---
+++ BeautifulUI/BeautifulUI/BeautifulUI/Views/SciodeskPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using BeautifulUI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BeautifulUI.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SciodeskPage : ContentPage
+    {
+        public SciodeskPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            var vm = BindingContext as SciodeskViewModel;
+            vm.SkeletonCommand.Execute(null);
+        }
+    }
+}
