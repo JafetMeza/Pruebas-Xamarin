2ab20b8bb3fcdc15c245e32f5260d6e6a35daae7
Abraham Meza <abraham.meza@sciodesk.com>
2020-05-27 10:00:13 -0500

    Aplicados recursos de FontAwesome

---
+++ BeautifulUI/BeautifulUI/BeautifulUI/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using BeautifulUI.Services;
+using BeautifulUI.Views;
+
+namespace BeautifulUI
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            //DependencyService.Register<MockDataStore>();
+            //MainPage = new AppShell();
+            MainPage = new SciodeskPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ BeautifulUI/BeautifulUI/BeautifulUI/AppShell.xaml.cs
@@ -0,0 +1,22 @@
+﻿using BeautifulUI.Views;
+using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace BeautifulUI
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            RegisterPages();
+        }
+
+        private void RegisterPages()
+        {
+            Routing.RegisterRoute("sciodesk", typeof(SciodeskPage));
+        }
+    }
+}
